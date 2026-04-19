891ca01d56eb657ffbbe0bd89d279631341250b7
Badis Meddouri <meddouri.badis@gmail.com>
2026-04-19 16:13:38 +0200

    Revert "Merge pull request #9 from meddouribadis/feature/bump-flutter"
    This reverts commit b958e25cb46105aefc5bb35727532254526ad554, reversing
    changes made to 4245722c190fd8f48e6a0bdb70879a2c4c0826bf.

--- lib/flauncher.dart
+++ lib/flauncher.dart
@@ -34,7 +34,7 @@ import 'package:flauncher/widgets/focus_aware_app_bar.dart';
 import 'package:flauncher/widgets/wallpaper_video_background.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flauncher/l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'models/app.dart';
 import 'models/category.dart';
